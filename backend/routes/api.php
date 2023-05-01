d2f09130634501ae217dd2e20fdf7068a7cacdf2
Rodolfo Win <rndcsjj@gmail.com>
2023-05-01 17:05:54 -0300

    cria rota para login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
-
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +16,24 @@ use App\Http\Controllers\Api\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'authorized'
+
+], function ($router) {
+
+    $router->group(['prefix' => 'auth'], function ($router) {
+        $router->post('/login', [LoginController::class, 'login']);
+    });
+ ;
+
 });
 
 $router->group(['prefix' => 'user'], function ($router) {
     $router->post('/store', [UserController::class, 'store']);
     $router->post('/update/{id}', [UserController::class, 'update']);
 });
-
